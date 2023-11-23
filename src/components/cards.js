5b4a80ce97288ea6844f4a92584c4b5e270794f8
Jinhyuk <kobin1970@gmail.com>
2023-11-23 20:38:58 +0900

    Resize home-Cards , Delete Trash CSS folder

--- src/components/cards.js
+++ src/components/cards.js
@@ -18,7 +18,6 @@ export default function Cards({ numOfCards = 4, flexWrapClass = false }) {
 
   return (
     <ul
-      style={flexWrapClass ? { flexWrap: "wrap" } : { flexWrap: "nowrap" }}
       className={
         flexWrapClass
           ? "flex flex-wrap justify-around w-full my-4 content-around"
@@ -28,7 +27,12 @@ export default function Cards({ numOfCards = 4, flexWrapClass = false }) {
       {todaysHit.map((element, idx) => {
         if (idx >= numOfCards) return null;
         return (
-          <li className=" w-60" key={idx}>
+          <li
+            className=""
+            key={idx}
+            style={{ marginRight: idx < 3 ? "1rem" : "" }}
+            i
+          >
             <figure className="w-[var(--album-width)] block">
               <img
                 className="block w-full object-cover rounded-[clamp(0.5rem,calc(0.5rem+(1024px-100vw)*1/4),3.125rem)] mb-4"
